3f31a602e8518c221a58987af40e6e8cdf1a9dac
Teloconsigouy <juansay4025@gmail.com>
2026-05-08 20:45:41 -0300

    Update start.js

--- start.js
+++ start.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const crypto = require("crypto");
 
 const app = express();
 
@@ -10,36 +11,61 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 function readJSON(file) {
-  return JSON.parse(fs.readFileSync(path.join(__dirname, file), "utf8"));
+  return JSON.parse(
+    fs.readFileSync(path.join(__dirname, file), "utf8")
+  );
+}
+
+function verifyPassword(password, salt, hash) {
+  const testHash = crypto
+    .pbkdf2Sync(password, salt, 100000, 64, "sha512")
+    .toString("hex");
+
+  return testHash === hash;
 }
 
 app.post("/api/login", (req, res) => {
   try {
     const { username, password } = req.body;
 
-    const users = readJSON("users.json");
+    const data = readJSON("users.json");
 
-    const user = users.find(
-      (u) => u.username === username && u.password === password
+    const user = data.users.find(
+      (u) => u.username === username
     );
 
     if (!user) {
       return res.status(401).json({
         success: false,
-        message: "Usuario o contraseña incorrectos",
+        message: "Usuario incorrecto",
       });
     }
 
-    res.json({
+    const valid = verifyPassword(
+      password,
+      user.salt,
+      user.hash
+    );
+
+    if (!valid) {
+      return res.status(401).json({
+        success: false,
+        message: "Contraseña incorrecta",
+      });
+    }
+
+    return res.json({
       success: true,
       user: {
         username: user.username,
-        role: user.role || "user",
+        role: user.role,
+        name: user.name,
       },
     });
   } catch (err) {
     console.error(err);
-    res.status(500).json({
+
+    return res.status(500).json({
       success: false,
       message: "Error interno",
     });
